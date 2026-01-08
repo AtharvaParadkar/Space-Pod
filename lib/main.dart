d4ad7a4bb7702fb43d566954ae6ce4b7c0673ee2
AtharvaParadkar <atharva.paradkar@omni-bridge.net>
2026-01-08 10:48:54 +0530

    Initial Project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(SpacePod());
+}
+
+class SpacePod extends StatelessWidget {
+  const SpacePod({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
